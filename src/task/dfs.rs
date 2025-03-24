344a49ffae9ebd61eeeddbaca1b5979d9fede3fa
Sachin Charakhwal <sachincharakhwal@gmail.com>
2025-03-24 10:04:20 +0530

    dfs: produce directories after all their are produced

--- src/task/dfs.rs
+++ src/task/dfs.rs
@@ -7,12 +7,17 @@ use crate::types::File;
 use super::TaskResult;
 
 pub(crate) struct Dfs {
-	stack: Vec<File>,
+	stack: Vec<Node>,
+}
+
+struct Node {
+	file: File,
+	visited: bool,
 }
 
 impl Dfs {
 	pub fn new(files: Vec<File>) -> Self {
-		Self { stack: files }
+		Self { stack: files.into_iter().map(|file| Node { file, visited: false }).collect() }
 	}
 }
 
@@ -20,25 +25,29 @@ impl Iterator for Dfs {
 	type Item = TaskResult<File>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		let file = self.stack.pop()?;
+		let node = self.stack.pop()?;
 
-		if !file.is_dir() {
-			return Some(Ok(file));
+		if node.visited || !node.file.is_dir() {
+			return Some(Ok(node.file));
 		}
 
-		let read_dir = match fs::read_dir(&file.path) {
+		let read_dir = match fs::read_dir(&node.file.path) {
 			Ok(read_dir) => read_dir,
 			Err(err) => return Some(Err(err.into())),
 		};
 
 		let read_dir = read_dir
 			.par_bridge()
+			// TODO: Do not ignore errors.
 			.filter_map(|d| d.ok())
+			// TODO: Do not ignore errors.
 			.map(|d| File::from_path(d.path()))
-			.filter_map(|res| res.ok());
+			.filter_map(|res| res.ok())
+			.map(|file| Node { file, visited: false });
 
+		self.stack.push(Node { file: node.file, visited: true });
 		self.stack.par_extend(read_dir);
 
-		Some(Ok(file))
+		self.next()
 	}
 }
